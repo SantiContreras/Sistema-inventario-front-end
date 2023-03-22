ca5b0a2ee821bd55a96e1d8b3f49caeb195df13e
santi <santiagolasconi@gmail.com>
2023-03-22 20:22:30 -0300

    Integracion con keycloak

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,31 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { KeycloakService ,  KeycloakAngularModule } from 'keycloak-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardModule } from './modules/dashboard/dashboard.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from '@angular/material/input';
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8082/',
+        realm: 'inventory',
+        clientId: 'angular-inventory'
+      },
+      initOptions: {
+        onLoad: 'login-required',
+        flow:"standard",
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      },
+      loadUserProfileAtStartUp:true,
+      
+    });
+}
 
 
 @NgModule({
@@ -21,10 +40,19 @@ import { MatInputModule } from '@angular/material/input';
     DashboardModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    KeycloakAngularModule
 
   ],
-  providers: [],
+  
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
